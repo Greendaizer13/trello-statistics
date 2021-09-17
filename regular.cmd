aa52ee4677515bd2cc608b567943086d7030a1b0
Olga Saveleva <fedchun13@yandex.ru>
2021-09-17 11:00:21 +0300

    Update

---
+++ regular.cmd
@@ -0,0 +1,7 @@
+cd C:\Users\fedch\source\repos\TrelloDataExporter\TrelloDataExporter\bin\Debug\netcoreapp3.1\
+TrelloDataExporter.exe
+cd C:\Docs\trello-statistics\trello-statistics
+git init
+git add .
+git commit -m Update
+git push
